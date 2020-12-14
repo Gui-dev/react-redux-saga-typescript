6627d1f7ee5f04456638b94620c77304c3df898f
Gui-dev <adrianon10@gmail.com>
2020-12-14 03:19:31 -0300

    feat: add rootSaga and config middlewares

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,16 @@
+import createSagaMiddleware from 'redux-saga'
+import { Middleware } from 'redux'
+
 import rootReducer from './modules/rootReducer'
+import { rootSaga } from './modules/rootSaga'
 import createStore from './createStore'
 
-const store = createStore(rootReducer, [])
+const sagaMiddleware = createSagaMiddleware()
+const middlewares: Middleware[] = [sagaMiddleware]
+
+const store = createStore(rootReducer, middlewares)
+
+sagaMiddleware.run(rootSaga)
 
 export {
   store
