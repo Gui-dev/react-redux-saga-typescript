3d0eb5bec4511212100017e738f8d328e853538d
Gui-dev <adrianon10@gmail.com>
2020-12-15 03:08:45 -0300

    feat: add cases signInSuccess and signInFailure

--- src/store/modules/auth/reducer.ts
+++ src/store/modules/auth/reducer.ts
@@ -8,15 +8,33 @@ export const TYPES_AUTH = {
 
 const INITIAL_STATE: IAuthState = {
   loadingSignInRequest: false,
-  isSignedIn: false
+  isSignedIn: false,
+  error: false,
+  token: null 
 }
 
 export const auth = (state = INITIAL_STATE, action: AuthAction): IAuthState => {
+
   switch (action.type) {
     case TYPES_AUTH.signInRequest:
       return {
         ...state,
         loadingSignInRequest: true
+      }
+
+    case TYPES_AUTH.signInSuccess:
+      return {
+        ...state,
+        loadingSignInRequest: false,
+        isSignedIn: true,
+        // token: action.payload.token,
+      }
+
+    case TYPES_AUTH.signInFailure:
+      return {
+        ...state,
+        loadingSignInRequest: false,
+        error: true
       }
   
     default:
